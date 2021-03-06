65bed1723742af2b5034fcf36d14f18c0bd862b1
Adarsh Sharma <s.adarsh1996@gmail.com>
2021-03-06 16:26:31 +0530

    bug fix

--- src/components/cashier/reports/CashierReportPage.js
+++ src/components/cashier/reports/CashierReportPage.js
@@ -370,6 +370,9 @@ const CashierReportPage = (props) => {
                   </h3>
                 )}
         </Card>
+        <Card marginBottom="20px" buttonMarginTop="32px" smallValue style={{height:'80px'}}>
+          <h4 style={{textAlign:'center'}}>Report generated at {`${new Date(formdate).getDay()}/${new Date(formdate).getMonth()+1}/${new Date(formdate).getFullYear()} ${new Date(formdate).getHours()}:${new Date(formdate).getMinutes()}`} </h4>
+        </Card>
       </Container>
       <Footer bankname={bankName} banklogo={bankLogo}/>
     </Fragment>
