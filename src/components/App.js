63e6bab1072a99a789c8f8a9b533c60b7690a997
beyondewallet <58459451+beyondewallet@users.noreply.github.com>
2022-06-15 14:48:37 +0530

    Few color changes

--- src/components/App.js
+++ src/components/App.js
@@ -104,16 +104,16 @@ import MerchantPayBills from './cashier/MerchantPayBills/CashierMerchantListPage
 WebFont.load({ google: { families: ['Roboto: 200,300,400,500'] } });
 
 const appTheme = {
-  primary: '#417505',
+  primary: '#316B83',
   // primary: "#ff0000",
   // secondary: "#ff0000",
-  secondary: '#6cac69',
+  secondary: '#6D8299',
   accent: '#f5a623',
   danger: '#f52828',
   light: '#9ea0a5',
   greyLine: '#666565 ',
-  vGradient: 'linear-gradient(to bottom, #6cac6a, #102910)',
-  hGradient: 'linear-gradient(to right, #6cac6a 1%, #102910)',
+  vGradient: 'linear-gradient(to bottom, #6D8299, #316B83)',
+  hGradient: 'linear-gradient(to right, #6D8299 1%, #316B83)',
   font: 'Roboto',
   fontSize: '14px',
   background: '#f5f5f5',
@@ -122,13 +122,13 @@ const appTheme = {
 const muiTheme = createMuiTheme({
   palette: {
     primary: {
-      main: '#417505',
+      main: '#316B83',
       hover: '#264503',
     },
     white: 'white',
-    main: '#417505',
-    vGradient: 'linear-gradient(to bottom, #6cac6a, #102910)',
-    hGradient: 'linear-gradient(to right, #6cac6a 1%, #102910)',
+    main: '#316B83',
+    vGradient: 'linear-gradient(to bottom, #6D8299, #316B83)',
+    hGradient: 'linear-gradient(to right, #6D8299 1%, #316B83)',
     fontSize: '14px',
   },
   status: {
